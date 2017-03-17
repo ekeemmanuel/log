56fc33ee708c1829bfb23cf34d3cb53d9bdcf433
1600558 <a@a.com>
2017-03-17 15:27:26 +0000

    Initial commit  frsn

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1600558
+ * Date: 17/03/2017
+ * Time: 15:03
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+</head>
+<body>
+            <form method="post" action="submit.php">
+                    <label for="username"> Username: <input type="text" name="username" placeholder="Enter Username"/></label>
+                    <label for="password">  Password: <input  type="password" name="pass" placeholder="Enter Password"/></label>
+                    <input type="submit" value="Login"/>
+
+
+
+            </form>
+</body>
+</html>
---
+++ submit.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1600558
+ * Date: 17/03/2017
+ * Time: 15:16
+ */
+$user=$_POST['username'];
+$password = $_POST['password'];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>submit</title>
+</head>
+<body>
+
+         <?php echo $user ?>
+         <?php echo $password ?>
+
+</body>
+</html>
